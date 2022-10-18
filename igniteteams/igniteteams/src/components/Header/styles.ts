ce7a78a9e6b5557e8d3bcab4786f4a3e9b84615e
unknown <jcarsan1000@gmail.com>
2022-10-18 08:01:54 -0300

    Criando o componente Highlight

--- igniteteams/igniteteams/src/components/Header/styles.ts
+++ igniteteams/igniteteams/src/components/Header/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
-import { CareLeft } from 'phosphor-react-native';
+import { CaretLeft } from 'phosphor-react-native';
+//import { AiOutlineCaretLeft } from 'react-icons/ai';
 
 export const Container = styled.View`
     width: 100%;
@@ -17,7 +18,7 @@ export const BackButton = styled.TouchableOpacity`
     flex: 1;
 `;
 
-export const BackIcon = styled(CareLeft).attrs( ({ theme }) => ({
+export const BackIcon = styled(CaretLeft).attrs( ({ theme }) => ({
     size: 32,
     color: theme.COLORS.WHITE
 }))`
